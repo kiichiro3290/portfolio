fb961755f3dce5b23aab3e9dc5b2cadd8976fb7f
kiichiro3290 <kai.kiichiro.418@s.kyushu-u.ac.jp>
2023-02-04 20:14:23 +0900

    take space

--- src/component/pages/note/note.tsx
+++ src/component/pages/note/note.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box } from '@mui/material'
+import { Box, Container } from '@mui/material'
 import { useSelector } from 'react-redux'
 import { selectTheme } from '~/store/theme'
 import { ArticleCard } from './parts/ArticleCard'
@@ -8,7 +8,16 @@ export const NotePage: React.FC = () => {
   const theme = useSelector(selectTheme)
   return (
     <Box component='div' width='100vw' height='100vh'>
-      <ArticleCard />
+      <Box
+        component='div'
+        sx={{
+          height: { md: theme.spacing(42), sm: theme.spacing(24) },
+          backgroundColor: theme.palette.background.paper,
+        }}
+      ></Box>
+      <Container maxWidth='md' sx={{ pt: theme.spacing(8) }}>
+        <ArticleCard />
+      </Container>
     </Box>
   )
 }
