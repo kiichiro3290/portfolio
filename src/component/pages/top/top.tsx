31ddd517721b7b1452f4ac97da86a6b41534c21d
kiichiro3290 <kai.kiichiro.418@s.kyushu-u.ac.jp>
2023-02-04 17:04:36 +0900

    create application pop over

--- src/component/pages/top/top.tsx
+++ src/component/pages/top/top.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import {
   Avatar,
   Box,
+  Grid,
+  IconButton,
   InputBase,
   Link,
   Paper,
-  TextField,
+  Popover,
   Typography,
 } from '@mui/material'
-import { Search } from '@mui/icons-material'
+import { NoteOutlined, Search } from '@mui/icons-material'
 import { useSelector } from 'react-redux'
 import { selectTheme } from '~/store/theme'
 import { Container } from '@mui/material'
@@ -28,89 +30,8 @@ export const TopPage: React.FC = () => {
           gap: theme.spacing(2),
         }}
       >
-        <Box
-          component='div'
-          sx={{
-            px: theme.spacing(2),
-            width: '100%',
-            height: theme.spacing(4),
-
-            borderBottom: 'solid',
-            borderWidth: theme.spacing(0.2),
-            borderColor: theme.palette.divider,
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-          }}
-        >
-          <Link
-            href='https://github.com/kiichiro3290'
-            target='_blank'
-            rel='noreferrer'
-            underline='none'
-          >
-            <Box
-              component='div'
-              sx={{
-                display: 'flex',
-                flexDirection: 'row',
-                gap: theme.spacing(1),
-              }}
-            >
-              <Image
-                src='/images/GitHub-Mark-32px.png'
-                alt=''
-                width='16px'
-                height='16px'
-              />
-              <Typography
-                sx={{
-                  fontSize: theme.spacing(1),
-                  color: theme.palette.text.primary,
-                }}
-              >
-                kiichiro3290
-              </Typography>
-            </Box>
-          </Link>
-        </Box>
-
-        <Box
-          sx={{
-            px: theme.spacing(1),
-            width: '100vw',
-            height: theme.spacing(4),
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'flex-end',
-            gap: theme.spacing(2),
-          }}
-        >
-          <Typography
-            sx={{
-              fontSize: theme.spacing(2),
-            }}
-          >
-            mail
-          </Typography>
-          <Typography
-            sx={{
-              fontSize: theme.spacing(2),
-            }}
-          >
-            画像
-          </Typography>
-          <Image
-            src='/images/nine-dots.svg'
-            alt=''
-            height='16px'
-            width='16px'
-          />
-          <Avatar>
-            <Typography>K</Typography>
-          </Avatar>
-        </Box>
+        <BookMarkBar />
+        <ApplicationBar />
       </Box>
 
       <Container
@@ -134,26 +55,219 @@ export const TopPage: React.FC = () => {
             width='300px'
           />
         </Box>
-        <Paper
-          sx={{
-            display: 'flex',
-            alignItems: 'center',
-            height: theme.spacing(4),
-            p: theme.spacing(2.5),
-            borderRadius: theme.spacing(4),
-            width: '100%',
-            gap: theme.spacing(2),
-          }}
-        >
-          <Search />
-          <InputBase
-            sx={{
-              color: theme.palette.text.primary,
-            }}
-            placeholder='検索または URL を入力'
-          />
-        </Paper>
+        <SearchWindow />
       </Container>
     </Box>
+  )
+}
+
+export const SearchWindow: React.FC = () => {
+  const theme = useSelector(selectTheme)
+  return (
+    <Paper
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        height: theme.spacing(4),
+        p: theme.spacing(2.5),
+        borderRadius: theme.spacing(4),
+        width: '100%',
+        gap: theme.spacing(2),
+      }}
+    >
+      <Search />
+      <InputBase
+        sx={{
+          color: theme.palette.text.primary,
+        }}
+        placeholder='検索または URL を入力'
+      />
+    </Paper>
+  )
+}
+
+export const BookMarkBar: React.FC = () => {
+  const theme = useSelector(selectTheme)
+  return (
+    <Box
+      component='div'
+      sx={{
+        px: theme.spacing(2),
+        width: '100%',
+        height: theme.spacing(4),
+
+        borderBottom: 'solid',
+        borderWidth: theme.spacing(0.2),
+        borderColor: theme.palette.divider,
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+      }}
+    >
+      <Link
+        href='https://github.com/kiichiro3290'
+        target='_blank'
+        rel='noreferrer'
+        underline='none'
+      >
+        <Box
+          component='div'
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            gap: theme.spacing(1),
+          }}
+        >
+          <Image
+            src='/images/GitHub-Mark-32px.png'
+            alt=''
+            width={theme.spacing(2)}
+            height={theme.spacing(2)}
+          />
+          <Typography
+            sx={{
+              fontSize: theme.spacing(1),
+              color: theme.palette.text.primary,
+            }}
+          >
+            kiichiro3290
+          </Typography>
+        </Box>
+      </Link>
+    </Box>
+  )
+}
+
+export const ApplicationBar: React.FC = () => {
+  const [anchorPopOver, setAnchorPopOver] = useState<HTMLButtonElement | null>(
+    null
+  )
+  const handleClickPopOver = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorPopOver(e.currentTarget)
+  }
+  const handleClosePopOver = () => {
+    setAnchorPopOver(null)
+  }
+  const openPopOver = Boolean(anchorPopOver)
+
+  const [anchorAppPopOver, setAnchorAppPopOver] =
+    useState<HTMLButtonElement | null>(null)
+  const handleClickAppPopOver = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorAppPopOver(e.currentTarget)
+  }
+  const handleCloseAppPopOver = () => {
+    setAnchorAppPopOver(null)
+  }
+  const openAppPopOver = Boolean(anchorAppPopOver)
+
+  const theme = useSelector(selectTheme)
+
+  return (
+    <Box
+      sx={{
+        px: theme.spacing(1),
+        width: '100vw',
+        height: theme.spacing(4),
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        gap: theme.spacing(2),
+      }}
+    >
+      <Typography
+        sx={{
+          fontSize: theme.spacing(2),
+        }}
+      >
+        mail
+      </Typography>
+      <Typography
+        sx={{
+          fontSize: theme.spacing(2),
+        }}
+      >
+        画像
+      </Typography>
+      <IconButton onClick={handleClickAppPopOver} size='large'>
+        <Image src='/images/nine-dots.svg' alt='' height='16px' width='16px' />
+      </IconButton>
+      <Popover
+        anchorEl={anchorAppPopOver}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        onClose={handleCloseAppPopOver}
+        open={openAppPopOver}
+      >
+        <ApplicationMenuPopOver />
+      </Popover>
+
+      <IconButton onClick={handleClickPopOver}>
+        <Avatar>
+          <Typography>K</Typography>
+        </Avatar>
+      </IconButton>
+      <Popover
+        anchorEl={anchorPopOver}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        onClose={handleClosePopOver}
+        open={openPopOver}
+      >
+        <AccountMenuPopOver />
+      </Popover>
+    </Box>
+  )
+}
+
+export const AccountMenuPopOver: React.FC = () => {
+  const theme = useSelector(selectTheme)
+
+  return (
+    <Paper sx={{ p: theme.spacing(2) }}>
+      <Typography>準備中...</Typography>
+    </Paper>
+  )
+}
+
+export const ApplicationMenuPopOver: React.FC = () => {
+  const theme = useSelector(selectTheme)
+  return (
+    <Paper sx={{ p: theme.spacing(2) }}>
+      <Grid container>
+        <Grid item>
+          {/* <IconButton
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              gap: theme.spacing(1),
+            }}
+          >
+            <Avatar>
+              <Typography>K</Typography>
+            </Avatar>
+            <Typography>アカウント</Typography>
+          </IconButton> */}
+        </Grid>
+        <Grid item>
+          <IconButton
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              gap: theme.spacing(1),
+            }}
+          >
+            <Avatar>
+              <NoteOutlined />
+            </Avatar>
+            <Typography>記事</Typography>
+          </IconButton>
+        </Grid>
+      </Grid>
+    </Paper>
   )
 }
