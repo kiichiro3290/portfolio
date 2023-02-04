6026ef53c14a2df463d20ecf2faa83ae6ca73b3c
kiichiro3290 <kai.kiichiro.418@s.kyushu-u.ac.jp>
2023-02-04 17:59:40 +0900

    add head tag into top page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Head from 'next/head'
 import { Provider } from 'react-redux'
 import { getBaseLayout } from '~/component/layout/base'
 
@@ -9,6 +10,11 @@ import { NextPageWithLayout } from '~/types/next'
 const Top: NextPageWithLayout = () => {
   return (
     <Provider store={store}>
+      <Head>
+        <title>Home</title>
+        <meta content='HOME' name='HOME' />
+        <link href='/images/nine-dots.svg' rel='icon' />
+      </Head>
       <TopPage />
     </Provider>
   )
