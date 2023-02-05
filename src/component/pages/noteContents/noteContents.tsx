10ccb90839ee32ce3366c585af1025b5ddc564c3
kiichiro3290 <kai.kiichiro.418@s.kyushu-u.ac.jp>
2023-02-05 15:11:15 +0900

    implement component ui

--- src/component/pages/noteContents/noteContents.tsx
+++ src/component/pages/noteContents/noteContents.tsx
@@ -1,5 +1,14 @@
 import React, { useEffect, useState } from 'react'
-import { Box, Chip, Container, Paper, Typography } from '@mui/material'
+import {
+  Alert,
+  Box,
+  Chip,
+  Container,
+  Divider,
+  Link,
+  Paper,
+  Typography,
+} from '@mui/material'
 import { useSelector } from 'react-redux'
 import { selectTheme } from '~/store/theme'
 import { BlockObjectResponse } from '@notionhq/client/build/src/api-endpoints'
@@ -15,15 +24,120 @@ const convertNotionWithReactComponent = (type: BlockType, content: string) => {
   switch (type) {
     case 'heading_1': {
       const res = (
-        <Typography variant='h5' component='h2'>
+        <Box
+          component='div'
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '8px',
+          }}
+        >
+          <Typography variant='h4' component='h2'>
+            {content}
+          </Typography>
+          <Divider />
+        </Box>
+      )
+      return res
+    }
+    case 'heading_2': {
+      const res = (
+        <Typography variant='h5' component='h3'>
           {content}
         </Typography>
       )
       return res
     }
-    case 'heading_2':
-      break
-    case 'heading_3':
+    case 'heading_3': {
+      const res = (
+        <Typography variant='h6' component='h4'>
+          {content}
+        </Typography>
+      )
+      return res
+    }
+    case 'paragraph': {
+      const res = (
+        <Typography variant='body1' component='p'>
+          {content}
+        </Typography>
+      )
+      return res
+    }
+    case 'numbered_list_item': {
+      const res = (
+        <ol>
+          <li>{content}</li>
+        </ol>
+      )
+      return res
+    }
+    case 'bulleted_list_item': {
+      const res = (
+        <ul>
+          <li>{content}</li>
+        </ul>
+      )
+      return res
+    }
+    case 'to_do': {
+      const res = (
+        <ul>
+          <li>{content}</li>
+        </ul>
+      )
+      return res
+    }
+    case 'divider': {
+      const res = <Divider />
+      return res
+    }
+    case 'quote': {
+      const res = (
+        <Box
+          sx={{
+            borderLeft: 'solid',
+            pl: '8px',
+          }}
+        >
+          <Typography component='p' variant='body1'>
+            {content}
+          </Typography>
+        </Box>
+      )
+      return res
+    }
+    case 'callout': {
+      const res = (
+        <Alert severity='info'>
+          <Typography component='p' variant='body1'>
+            {content}
+          </Typography>
+        </Alert>
+      )
+      return res
+    }
+    case 'code': {
+      const res = (
+        <Alert severity='info'>
+          <Typography component='p' variant='body1'>
+            {content}
+          </Typography>
+        </Alert>
+      )
+      return res
+    }
+    case 'bookmark': {
+      const res = (
+        <Link href={content} underline='none'>
+          ブックマーク
+        </Link>
+      )
+      return res
+    }
+    default: {
+      return null
+    }
   }
 }
 
@@ -89,7 +203,10 @@ export const NoteContentsPage: React.FC<NoteContentsPageProps> = ({
             blocks.map((block, id) => {
               return (
                 <Box key={`${id}${block.content}`} component='div'>
-                  {convertNotionWithReactComponent(block.type, block.content)}
+                  {convertNotionWithReactComponent(
+                    block.type,
+                    block.content ?? ''
+                  )}
                 </Box>
               )
             })}
